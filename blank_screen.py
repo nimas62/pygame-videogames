33c5630803057e0250d1cb192aa4f21febc8e9e0
Nima Sanjabi <nima.sanjabi@gmail.com>
2019-04-24 17:51:17 +0200

    Adding the blank_screen.py

---
+++ blank_screen.py
@@ -0,0 +1,31 @@
+"""
+an initial blank screen for a pygame video game
+
+@author: Nima Created on Wed Apr 24 16:14:45 2019
+"""
+
+import pygame
+   
+def main():
+    """this function is called when the program starts.
+       it initializes everything it needs, then runs in
+       a loop until the function returns."""
+    #Initialize Everything     
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480))
+    clock=pygame.time.Clock()
+    
+    # main loop
+    while True: 
+        # a clock keep the frame rate under 60
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        pygame.display.update()
+    pygame.quit()
+    
+# game main loop
+if __name__=="__main__":
+    main()
+        
