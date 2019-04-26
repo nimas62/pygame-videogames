c40481a2dc698db7cbd76d834642a07244ba0922
Nima Sanjabi <nima.sanjabi@gmail.com>
2019-04-26 03:24:18 +0200

    Before testing blocks tutorial

--- moving_shape.py
+++ moving_shape.py
@@ -8,41 +8,38 @@ Created on Wed Apr 24 16:14:45 2019
 import pygame
 import sys
 from lib import utils
+from pygame.locals import *
+from pygame.compat import geterror
 
 if not pygame.font: print ('Warning, fonts disabled')
 
 # game constants
-NOMINAL_FRAME_RATE = 12000
+NOMINAL_FRAME_RATE = 60
 SCREEN_WIDTH, SCREEN_HEIGHT = 480, 300
-SJAPE_WIDTH, SHAPE_HEIGHT = 64, 64
-
-BG_COLOR=(0, 0, 0)
+SHAPE_WIDTH, SHAPE_HEIGHT = 64, 64
+#X_MOVE, Y_MOVE = 1, 1
+    
+BG_COLOR=(255, 0, 0)
 SHAPES_COLOR = (136, 187, 0)
 TEXT_COLOR = (17, 119, 136)
     
-class MovingShape():
-    """Defines a rectangle shape."""
-    def __init__(self,left, top, width, height):
-        self.shape_rect=pygame.Rect(left, top, width, height)
-        self.color=(SHAPES_COLOR)
-        self.screen = pygame.display.get_surface()
-        print('screen', self.screen)
+class MovingShape(pygame.sprite.Sprite):
+    """Defines a shape."""
+    def __init__(self):
+        # calls the Sprite initializer
+        pygame.sprite.Sprite.__init__(self)
+        self.shape_rect=pygame.Rect(0, 0, SHAPE_WIDTH, SHAPE_HEIGHT)
+        self.shape_surface=pygame.Surface([SHAPE_WIDTH, SHAPE_HEIGHT])
+        self.color=(0, 255, 0)
+        #self.screen = pygame.display.get_surface()
+        #print('screen', self.screen)
         
-    def set_pos_size(self,left, top, width, height):
-        """sets the position of the top left corrner of
-        the player rectangle."""
-        self.shape_rect=pygame.Rect(left, top, width, height)
-
-    def display_shape(self, color, rect):
-        """draw the initiated shape."""        
-        pygame.draw.rect(self.screen, color, rect)
         
-    def move_shape(self, x_move, y_move):
+    def move_shape(self, X_MOVE, Y_MOVE):
         """move the shape by x and y steps. Before each 
         move, the previous position on the last frame is
-        repainted()erased with black color."""
-        self.display_shape(BG_COLOR, self.shape_rect)           
-        self.shape_rect=self.shape_rect.move(x_move,y_move)
+        repainted()erased with black color."""         
+        self.shape_rect=self.shape_rect.move(X_MOVE,Y_MOVE)
   
 def main():
     """this function is called when the program starts.
@@ -51,12 +48,13 @@ def main():
        
     #Initialize Everything     
     pygame.init()
-    x_move, y_move = 1, 1
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    screen.fill((255,0,0))
+    pygame.display.set_caption('Bouncing square!')    
     background = pygame.Surface(screen.get_size())
-    background.fill(BG_COLOR)
-    
+    background = background.convert()
+#    background.fill((255,255,0))
+    X_MOVE, Y_MOVE = 1, 1
     # a list as a buffer to save the recent samples of frame rate
     BUFFER_SIZE=10
     frame_rate_buffer=[0]*BUFFER_SIZE
@@ -64,10 +62,16 @@ def main():
     frame_counter=0
     clock=pygame.time.Clock()
     
+
+    #Display The Background
+#    screen.blit(background, (0, 0))
+#    pygame.display.flip()
+    
+    
     # creates a shape object from the MovingShape class
-    shape_1=MovingShape(0, 0, SJAPE_WIDTH, SHAPE_HEIGHT)
-    shape_1.set_pos_size(40,40,SJAPE_WIDTH,SHAPE_HEIGHT)
-    shape_1.display_shape(shape_1.color, shape_1.shape_rect)
+    shape_1=MovingShape()
+    #pygame.draw.rect(shape_1.screen, shape_1.color, shape_1.shape_rect)
+    allsprites = pygame.sprite.Group()
     
     # main loop
     while True: 
@@ -75,41 +79,45 @@ def main():
         clock.tick(NOMINAL_FRAME_RATE)
         frame_counter+=1
         #move the shape 1 step
-        shape_1.move_shape(x_move,y_move)
-        shape_1.display_shape(shape_1.color, shape_1.shape_rect)
-        pygame.display.update()
-
+        shape_1.move_shape(X_MOVE,Y_MOVE)
         # reverse the shape when it colides with the screen walls       
         rect_1=shape_1.shape_rect
         if rect_1.left < 0 or rect_1.right > SCREEN_WIDTH:
-            x_move = -x_move
+            X_MOVE = -X_MOVE
         if rect_1.top < 0 or rect_1.bottom > SCREEN_HEIGHT:
-            y_move = -y_move
+            Y_MOVE = -Y_MOVE
+            print('collision')
   
+        allsprites.update()
+#        """ displays the real frame rate which might be different
+#        from the nominal frame rate."""
+#        if frame_counter > 20:
+#            frame_counter=0
+#            if pygame.font:
+#                font = pygame.font.Font(None, 24)
+#                # erase the last text with black color
+#                text = font.render('Frame rate: {} fps'.format(str(round(last_frame_rate))), 1, BG_COLOR)
+#                textpos = pygame.Rect(10, 10, 140, 17)
+#                background.blit(text, textpos)
+#                screen.blit(background, (0, 0))
+#                
+#                #get the frame rate            
+#                frame_rate = utils.list_average(utils.shift_buffer(frame_rate_buffer, clock.get_fps()))
+#                #frame_rate = clock.get_fps()
+#                
+#                # write the frame rate on the screen with red color
+#                text = font.render('Frame rate: {} fps'.format(str(round(frame_rate))), 1, TEXT_COLOR)
+#                textpos = pygame.Rect(10, 10, 140, 17)
+#                background.blit(text, textpos)
+#                screen.blit(background, (0, 0))
+#                last_frame_rate = frame_rate
+         
+
+
+        #Draw Everything
+        allsprites.draw(screen)
+        pygame.display.flip()
         
-        """ displays the real frame rate which might be different
-        from the nominal frame rate."""
-        if frame_counter > 20:
-            frame_counter=0
-            if pygame.font:
-                font = pygame.font.Font(None, 24)
-                # erase the last text with black color
-                text = font.render('Frame rate: {} fps'.format(str(round(last_frame_rate))), 1, BG_COLOR)
-                textpos = pygame.Rect(10, 10, 140, 17)
-                background.blit(text, textpos)
-                screen.blit(background, (0, 0))
-                
-                #get the frame rate            
-                frame_rate = utils.list_average(utils.shift_buffer(frame_rate_buffer, clock.get_fps()))
-                #frame_rate = clock.get_fps()
-                
-                # write the frame rate on the screen with red color
-                text = font.render('Frame rate: {} fps'.format(str(round(frame_rate))), 1, TEXT_COLOR)
-                textpos = pygame.Rect(10, 10, 140, 17)
-                background.blit(text, textpos)
-                screen.blit(background, (0, 0))
-                last_frame_rate = frame_rate
-            
         # handles the exit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
